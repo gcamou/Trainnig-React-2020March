f571ecd5280e12167f215cb9271067863bba5eab
gCamou <gcamou@gCamouInc>
2020-04-01 23:49:09 -0600

    Finish exercises for axios

--- axios/src/containers/Blog/Blog.js
+++ axios/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,53 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(resp => {
+                const posts = resp.data.slice(0, 4);
+                const newPost = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({ posts: newPost })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ error: err });
+            });
+    }
+
+    postSelectedHandler = id => {
+        this.setState({ selectedPostId: id })
+    };
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Somethings was wrong...!</p>
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
