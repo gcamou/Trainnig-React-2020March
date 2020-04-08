d019ce495d7b914acfe590361427017e05f56775
gCamou <gcamou@gCamouInc>
2020-04-08 01:32:25 -0500

    redux basic functionability

---
+++ redux/src/store/reducers/result.js
@@ -0,0 +1,25 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    result: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                result: state.result.concat({ id: new Date(), value: action.result })
+            }
+        case actionTypes.DELETE_RESULT:
+            const newResult = state.result.filter(result => result.id !== action.id)
+            return {
+                ...state,
+                result: newResult
+            }
+    }
+
+    return state
+}
+
+export default reducer;
